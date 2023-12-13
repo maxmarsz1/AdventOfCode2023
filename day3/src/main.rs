762c600a88191eb579675b715599255fb652ead6
piayer69 <maxmarsz1@gmail.com>
2023-12-13 20:48:34 +0100

    Day 3 (finished)

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -8,11 +8,35 @@ struct Symbol {
 }
 
 impl Symbol {
-    // fn is_adjacent_to_two_num(&self, row: usize, numbers: &Vec<Number>) -> bool {
-    //     for i in row - 1..row + 2 {
-
-    //     }
-    // }
+    fn get_gear_ratio(&self, row: usize, numbers: &BTreeMap<usize, Vec<Number>>) -> i32 {
+        let mut neighbours: Vec<&Number> = Vec::new();
+        for y in row - 1..row + 2 {
+            if let Some(cols) = numbers.get(&y) {
+                let filtered: Vec<&Number> = cols
+                    .iter()
+                    .filter(|n| {
+                        (self.col - 1..self.col + 2).contains(&n.coord.col)
+                            || (self.col - 1..self.col + 2).contains(&(n.coord.col_end - 1))
+                    })
+                    .collect();
+                for num in filtered {
+                    if !neighbours.contains(&num) {
+                        neighbours.push(num);
+                    }
+                }
+                println!("Symbol: {} at {}x{}", self.sym, row, self.col);
+                println!("{:?}", neighbours);
+            }
+        }
+        let mut power = 1;
+        if neighbours.len() == 2 {
+            for n in neighbours {
+                power *= n.value;
+            }
+            return power;
+        }
+        0
+    }
 }
 
 #[derive(Debug)]
@@ -22,26 +46,34 @@ struct Coord {
     col_end: usize,
 }
 
+impl PartialEq for Coord {
+    fn eq(&self, other: &Self) -> bool {
+        self.row == other.row && self.col == other.col
+    }
+}
+
 #[derive(Debug)]
 struct Number {
     value: i32,
     coord: Coord,
 }
 
+impl PartialEq for Number {
+    fn eq(&self, other: &Self) -> bool {
+        self.value == other.value && self.coord == other.coord
+    }
+}
+
 impl Number {
-    fn is_adjacent_to_sym(&self, symbols: &BTreeMap<usize, Vec<Symbol>>) -> bool {
+    fn is_adjacent_to_sym(&self, row: usize, symbols: &BTreeMap<usize, Vec<Symbol>>) -> bool {
         let ixstart = if self.coord.col == 0 {
             0
         } else {
             self.coord.col - 1
         };
         let ixend = self.coord.col + self.value.to_string().len();
-        let iystart = if self.coord.row == 0 {
-            0
-        } else {
-            self.coord.row - 1
-        };
-        let iyend = self.coord.row + 1;
+        let iystart = if row == 0 { 0 } else { row - 1 };
+        let iyend = row + 1;
         println!("Searched area is: {ixstart}-{ixend}x{iystart}-{iyend}");
         for i in iystart..iyend + 1 {
             if let Some(cols) = symbols.get(&i) {
@@ -62,18 +94,18 @@ impl Number {
 fn main() {
     let input = fs::read_to_string("input").unwrap();
     let mut symbols: BTreeMap<usize, Vec<Symbol>> = BTreeMap::new();
-    let mut numbers: Vec<Number> = Vec::new();
+    // let mut numbers: Vec<Number> = Vec::new();
+    let mut numbers1: BTreeMap<usize, Vec<Number>> = BTreeMap::new();
     let mut sum = 0;
 
     //Collecting symbols cords
     for (r, line) in input.lines().enumerate() {
         for (c, ch) in line.chars().enumerate() {
             if !ch.is_ascii_digit() && ch != '.' {
+                let sym = Symbol { col: c, sym: ch };
+
                 symbols.entry(r).or_default();
-                symbols
-                    .get_mut(&r)
-                    .unwrap()
-                    .push(Symbol { col: c, sym: ch });
+                symbols.get_mut(&r).unwrap().push(sym);
                 println!("{ch}");
             }
         }
@@ -91,14 +123,24 @@ fn main() {
                     col_end: needle.end(),
                 },
             };
-            println!("{}", num.is_adjacent_to_sym(&symbols));
-            if num.is_adjacent_to_sym(&symbols) {
+            println!("{}", num.is_adjacent_to_sym(r, &symbols));
+            if num.is_adjacent_to_sym(r, &symbols) {
                 sum += num.value;
             }
-            numbers.push(num);
+            numbers1.entry(r).or_default();
+            numbers1.get_mut(&r).unwrap().push(num);
         }
     }
-    println!("{symbols:?}");
-    println!("{numbers:?}");
-    println!("{sum}");
+    let mut power = 0;
+    for (row, syms) in &symbols {
+        for sym in syms {
+            if sym.sym == '*' {
+                power += sym.get_gear_ratio(*row, &numbers1);
+            }
+        }
+    }
+    // println!("{symbols:?}");
+    // println!("{numbers1:?}");
+    println!("Part1: {sum}");
+    println!("Part2: {power}");
 }
